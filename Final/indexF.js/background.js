b7e721dd77065ee6bbc776798959049c513fd089
Kin9kong <103474785+Kin9kong@users.noreply.github.com>
2022-10-10 02:33:18 +0900

    Delete Final directory

--- Final/indexF.js/background.js
+++
@@ -1,9 +0,0 @@
-const images = ["0.jpeg", "1.jpeg", "2.jpg", "4.jpeg"];
-
-const chosenImage = images[Math.floor(Math.random() * images.length)];
-
-const bgImage = document.createElement("img");
-
-bgImage.src = `image/${chosenImage}`;
-
-document.body.appendChild(bgImage);
